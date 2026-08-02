dc735070d9546dbc6cf0c7755695cbaca1ae89bf
ILIAWM <psychomamba83@gmail.com>
2026-08-02 10:10:47 +0330

    added shutdown button  ui

--- components/TaskBar/Menu/MainMenu.tsx
+++ components/TaskBar/Menu/MainMenu.tsx
@@ -1,7 +1,106 @@
-
+import Image from "next/image";
+import { IoIosSearch } from "react-icons/io";
+import { RiArrowRightWideLine } from "react-icons/ri";
+import { FaPowerOff } from "react-icons/fa6";
+import { useState } from "react";
+import { MdRestartAlt } from "react-icons/md";
+import { IoMoonOutline } from "react-icons/io5";
 const MainMenu = () => {
+  const [menu,setMenu]=useState(false)
+  const [Mode,setMode]=useState(4)
+
+  const handleShutdown=()=>{
+    if(Mode===0){
+      
+    }
+
+  }
   return (
-    <div>
+    <div className="w-full h-full p-7 pb-0 flex flex-col gap-5">
+      
+        <section className="search px-3 w-full h-12 flex gap-4  items-center ring ring-gray-700/80 rounded-sm border-b-2 border-purple-700 ">
+          <button className="flex scale-x-[-1] cursor-pointer"><IoIosSearch/></button>
+          <input type="text" className="w-full h-full focus:outline-0 " placeholder="Type here to search"/>
+        </section>
+        <div className="w-full h-full flex flex-col justify-start gap-5 rounded-xl  ">
+          <section className="w-full h-fit min-h-30 flex flex-col ">
+          <div className="flex justify-between items-center px-5 py-3 ">
+            <h1>Pinned</h1>
+            <button className="flex gap-1 items-center bg-[#343434] px-2 py-1 rounded-lg cursor-pointer hover:scale-[1.07] active:scale-[1]">
+              <h3>All apps</h3>
+              <div className="icon "><RiArrowRightWideLine/></div>
+            </button>
+          </div>
+          {/* apps */}
+          <div>
+
+          </div>
+        </section>
+       <section className="w-full h-fit min-h-30 flex flex-col  ">
+          <div className="flex justify-between items-center px-5 py-3 ">
+            <h1>Recommended</h1>
+            <button className="flex gap-1 items-center bg-[#343434] px-2 py-1 rounded-lg cursor-pointer hover:scale-[1.07] active:scale-[1]">
+              <h3>More</h3>
+              <div className="icon"><RiArrowRightWideLine/></div>
+            </button>
+          </div>
+          {/* recents*/}
+          <div>
+
+          </div>
+        </section>
+        </div>
+        <div className="profile flex items-center justify-between  relative bottom-0 bg-[#1c1c1c] w-full h-20 px-3 mb-1">
+          <div className="User flex gap-2 items-center py-4">
+              <div className="w-8 h-8 relative"><Image  src={'/pfp/me.jpg'} fill className="rounded-full" alt="Userpfp"/></div>
+              <h3 className="font-bold">Iliawm</h3>
+          </div>
+          <div className="buttons flex justify-center">
+            
+              <button className="shotdown hover:bg-gray-600 transition-all duration-100 ease-linear px-3 py-2 cursor-pointer rounded-lg hover:scale-[1.06] active:scale-[1]" onClick={()=>{
+                      setMenu(!menu) 
+              }}>
+                  <FaPowerOff />
+              </button>
+              <div className={`absolute w-fit bg-[#1a1a1a] h-fit bottom-15 shadow shadow-gray-700/60 rounded-xl border border-gray-700/40 p-3 px-4 flex flex-col ${menu ? "opacity-100 " : "opacity-0 pointer-events-none"} text-sm border-gray-700/60`}>
+                <button className="shotdown flex gap-2.5 items-center text-nowrap cursor-pointer hover:scale-[1.05] active:scale-[1] transition-all ease-linear hover:bg-white/10 rounded-md px-1 py-1" onClick={()=>{
+                  setMode(2)
+                  handleShutdown()
+                }}>
+                  <span className="flex scale-x-[-1]">
+                    <IoMoonOutline />
+                    </span> 
+                  <div>
+                    sleep
+                     </div>
+                  </button>
+                <button className="shotdown flex gap-2.5 items-center text-nowrap cursor-pointer hover:scale-[1.05] active:scale-[1] transition-all ease-linear hover:bg-white/10 rounded-md px-1 py-1" onClick={()=>{
+                  setMode(0)
+                  handleShutdown()
+                }}>
+                  <span>
+                    <FaPowerOff />
+                    </span> 
+                  <div>
+                    Shut down
+                     </div>
+                  </button>
+                <button className="shotdown flex gap-2.5 items-center text-nowrap cursor-pointer hover:scale-[1.05] active:scale-[1] transition-all ease-linear hover:bg-white/10 rounded-md px-1 py-1" onClick={()=>{
+                  setMode(1)
+                  handleShutdown()
+                }}>
+                  <span>
+                   <MdRestartAlt />
+                  </span> 
+                  <div>
+                    Restart
+                     </div>
+                  </button>
+                
+              </div>
+          </div>
+        </div>
+        
         
     </div>
   )
--- components/TaskBar/TaskBar.tsx
+++ components/TaskBar/TaskBar.tsx
@@ -27,11 +27,11 @@ const TaskBar = () => {
     const renderMenu = () => {
         switch (menuIndex) {
             case 0:
-                return <div className="p-4"><MainMenu/></div>
+                return <div className="w-full h-full"><MainMenu/></div>
             case 1:
-                return <div className="p-4"><SearchMenu/></div>
+                return <div className="w-full h-full"><SearchMenu/></div>
             default:
-                return <div className="p-4"><MainMenu/></div>
+                return <div className="w-full h-full"><MainMenu/></div>
         }
     }
 
@@ -49,12 +49,12 @@ const TaskBar = () => {
 
     return (
         <section className={`w-full h-15 ${currentTheme === "dark" ? "bg-[#1A1A1A]" : "bg-[#FFFFFF]"} fixed bottom-0 flex z-40 justify-center items-center py-2 gap-2 invisible md:visible`}>
-            <div className={`absolute h-140 w-120 lg:w-170 lg:h-170 ${theme==="dark" ? "bg-[#1A1A1A]":"bg-white"} transition-all bottom-19 z-30 rounded-xl ${Menu ? "opacity-100 " : "translate-y-full pointer-events-none no opacity-0"}`}>
-                {renderMenu()}
+            <div className={`absolute h-140 w-120 lg:w-170 lg:h-170 ${theme==="dark" ? "bg-[#1A1A1A]":"bg-white"} transition-all bottom-19 z-30 rounded-xl ${Menu ? "opacity-100 translate-y-0" : "translate-y-4 opacity-0 pointer-events-none"}`}>
+            {renderMenu()}
             </div>
 
             {/* WINDOWS */}
-            <button className={` cursor-pointer w-12 h-full relative ${theme==="dark"?"hover:bg-gray-600":"hover:bg-gray-200"} rounded-lg hover:scale-[1.1] active:scale-[1]`} onClick={() => {
+            <button className={`${menuIndex===0 && Menu ? (theme==="dark"? "bg-gray-600": "bg-gray-200"):""} cursor-pointer w-12 h-full relative ${theme==="dark"?"hover:bg-gray-600":"hover:bg-gray-200"} rounded-lg hover:scale-[1.1] active:scale-[1]`} onClick={() => {
                 if (menuIndex !== 0 && Menu) {
                     SetMenu(true)
                     SetMenuIndex(0)
@@ -67,7 +67,7 @@ const TaskBar = () => {
             </button>
 
             {/* SEARCH */}
-            <button className={`cursor-pointer w-12 h-full relative ${theme==="dark"?"hover:bg-gray-600":"hover:bg-gray-200"} rounded-lg hover:scale-[1.1] active:scale-[1]`} onClick={() => {
+            <button className={`${menuIndex===1 && Menu ? (theme==="dark"? "bg-gray-600": "bg-gray-200"):""} cursor-pointer w-12 h-full relative ${theme==="dark"?"hover:bg-gray-600":"hover:bg-gray-200"} rounded-lg hover:scale-[1.1] active:scale-[1]`} onClick={() => {
                 if (menuIndex !== 1 && Menu) {
                     SetMenu(true)
                     SetMenuIndex(1)
